16d82371ce04b83a24f31ea25af0caaaa4086e79
ps2-controller <anurag.angara@gmail.com>
2019-01-07 13:31:50 -0500

    separate game out into its own file

---
+++ contracts/gamble-game.sol
@@ -0,0 +1,50 @@
+contract Gamble {
+	constructor (uint _costToEnter, address _host) public payable {
+        setGameAddresses(_host);
+        setCostToEnter(_costToEnter);
+    }
+	address host;
+	address player;
+	uint costToEnter;
+
+	modifier onlyHost {
+		require (msg.sender == host);
+		_;
+	}
+
+	function setGameAddresses (address _host) private {
+		host = _host;
+		player = 0x0000000000000000000000000000000000000000;
+	}
+
+	function setCostToEnter(uint _costToEnter) private {
+		costToEnter = _costToEnter;
+	}
+
+	function joinGame() public payable {
+		require (player == 0x0000000000000000000000000000000000000000);
+		require (msg.value == costToEnter);
+		player = msg.sender;
+	}
+
+	function startGame() public payable onlyHost {
+		require (player != 0x0000000000000000000000000000000000000000);
+
+		// randomness to determine winner - current implementation is insecure
+		uint winner = uint(keccak256(abi.encodePacked(now, msg.sender))) % 2;
+
+
+		if (winner == 1){
+			// can't figure out why there's a bug here
+			address(host).transfer(address(this).balance);
+		}
+		else {
+		    player.transfer(address(this).balance);
+		}
+
+	}
+
+	function getCostToEnter() public view returns (uint) {
+		return costToEnter;
+	}
+}

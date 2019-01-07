903144ee644e9111c3d53fb6ae7eb8d8f4e40484
ps2-controller <anurag.angara@gmail.com>
2019-01-07 13:31:27 -0500

    remove game from factory

---
+++ contracts/gamble-factory.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+contract GambleFactory{
+	event newGambleContractCreated(
+		address _host, string _gambleName);
+
+	function createGambleContract (string memory _gambleName, uint _costToEnter) public payable{
+		require (msg.value == _costToEnter);
+		Gamble newGamble = (new Gamble).value(_costToEnter)(_costToEnter, msg.sender);
+		emit newGambleContractCreated(msg.sender, _gambleName);
+	}
+}

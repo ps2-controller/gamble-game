5c84623a5bd701faf6fdaee8ea5846f2479d5d69
Anurag <anurag.angara@gmail.com>
2019-01-08 00:09:25 -0500

    storing games in an array

--- contracts/gamble-factory.sol
+++ contracts/gamble-factory.sol
@@ -2,12 +2,18 @@ pragma solidity ^0.5.0;
 import "./ gamble-game.sol"
 
 contract GambleFactory{
+
+	Gamble[] gambleGames;
+	mapping(address => Gamble[]) gambleGamesByHost;
+	mapping(address => Gamble[]) gambleGamesByPlayer;
+
 	event newGambleContractCreated(
 		address _host, string _gambleName);
 
 	function createGambleContract (string memory _gambleName, uint _costToEnter) public payable{
 		require (msg.value == _costToEnter);
 		Gamble newGamble = (new Gamble).value(_costToEnter)(_costToEnter, msg.sender);
+		gambleGames.push(newGamble);
 		emit newGambleContractCreated(msg.sender, _gambleName);
 	}
 }
